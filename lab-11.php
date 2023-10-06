6da82748c48133f2b51d19682a003583749b66db
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-06 21:36:05 +0545

    practical no 11 done

--- lab-11.php
+++ lab-11.php
@@ -1,1 +1,16 @@
 <!-- Write an example of passing argument and returning value in php -->
+
+<?php
+function addNumbers($num1, $num2) {
+    $sum = $num1 + $num2;
+    return $sum;
+}
+
+// Usage example
+$number1 = 5;
+$number2 = 7;
+
+$result = addNumbers($number1, $number2);
+
+echo "The sum of $number1 and $number2 is: $result";
+?>
