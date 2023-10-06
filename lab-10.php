fbc60e641d3307aabd4bd173c4574813c52bdfcb
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-06 21:27:15 +0545

    practical no 10 done

--- lab-10.php
+++ lab-10.php
@@ -1,1 +1,14 @@
 <!-- Find cube of given number using functions -->
+
+<?php
+function cube($number) {
+    $result = $number * $number * $number;
+    return $result;
+}
+
+// Usage example
+$number = 5; 
+$cubeResult = cube($number);
+
+echo "The cube of $number is: $cubeResult";
+?>
