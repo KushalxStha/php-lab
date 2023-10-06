78834b2a6ef396e853f74b51a14ca74db832cf8b
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-06 21:20:28 +0545

    practical no 9 done

--- lab-9.php
+++ lab-9.php
@@ -1,2 +1,18 @@
 <!-- Write your information using PHP functions -->
 
+<?php
+function displayMyInfo() {
+    $name = "Kushal";
+    $address = "Kathmandu";
+    $college = "Amrit Science Campus";
+    $course = "BIT";
+
+    echo "Name: $name<br>";
+    echo "Address: $address<br>";
+    echo "College: $college<br>";
+    echo "Course: $course<br>";
+}
+
+// Call the function to display the information
+displayMyInfo();
+?>
