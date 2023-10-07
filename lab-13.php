22393973a1d1be9106110af4494ec562e70942e4
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-07 21:04:10 +0545

    practical no 13 done

--- lab-13.php
+++ lab-13.php
@@ -1,1 +1,18 @@
 <!-- Write a PHP program to count the words in the string -->
+
+<?php
+function countWords($str) {
+    // Use the str_word_count function to count words in the string
+    $wordCount = str_word_count($str);
+    return $wordCount;
+}
+
+// Input string
+$inputString = "This is a sample sentence. It contains several words.";
+
+// Calling function to count words in the string
+$wordCount = countWords($inputString);
+
+echo "Input string: $inputString<br>";
+echo "Word count: $wordCount";
+?>
