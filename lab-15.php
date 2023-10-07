931b880ba80f1cb38cbc335865162f14c94fe659
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-07 21:12:17 +0545

    practical no 15 done

--- lab-15.php
+++ lab-15.php
@@ -1,1 +1,36 @@
 <!-- Create a class named "Bird". Also, create two objects pigeon and crow. Display their colors, habitats and what they eat. -->
+
+<?php
+class Bird {
+    public $name;
+    public $color;
+    public $habitat;
+    public $diet;
+
+    public function __construct($name, $color, $habitat, $diet) {
+        $this->name = $name;
+        $this->color = $color;
+        $this->habitat = $habitat;
+        $this->diet = $diet;
+    }
+
+    public function displayInfo() {
+        echo "Name: " . $this->name . "<br>";
+        echo "Color: " . $this->color . "<br>";
+        echo "Habitat: " . $this->habitat . "<br>";
+        echo "Diet: " . $this->diet . "<br>";
+    }
+}
+
+// Create objects
+$pigeon = new Bird("Pigeon", "Gray", "Urban areas", "Seeds and crumbs");
+$crow = new Bird("Crow", "Black", "Various habitats", "Insects, carrion, and small animals");
+
+// Display information
+echo "Pigeon:<br>";
+$pigeon->displayInfo();
+echo "<br>";
+
+echo "Crow:<br>";
+$crow->displayInfo();
+?>
