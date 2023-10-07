8465ed642a3b303ae2fc06366585d6b7be5fc795
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-07 21:09:22 +0545

    practical no 14 done

--- lab-14.php
+++ lab-14.php
@@ -1,1 +1,20 @@
 <!-- Remove specific element by value from an array in PHP -->
+
+<?php
+// Original array
+$myArray = array(1, 2, 3, 4, 5);
+
+// Value to remove
+$valueToRemove = 3;
+
+// Use array_filter to remove the element by value
+$filteredArray = array_filter($myArray, function ($element) use ($valueToRemove) {
+    return $element !== $valueToRemove;
+});
+
+// Convert the filtered array back to indexed keys
+$filteredArray = array_values($filteredArray);
+
+// Display the filtered array
+print_r($filteredArray);
+?>
