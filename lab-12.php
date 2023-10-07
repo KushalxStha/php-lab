89918902b529fb824cda8915abf92679a21cf24e
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-07 21:01:07 +0545

    practical no 12 done

--- lab-12.php
+++ lab-12.php
@@ -1,1 +1,23 @@
 <!-- Write a PHP program to reverse the string -->
+
+<?php
+function reverseString($str) {
+    $length = strlen($str);
+    $reversed = '';
+    
+    for ($i = $length - 1; $i >= 0; $i--) {
+        $reversed .= $str[$i];
+    }
+    
+    return $reversed;
+}
+
+// Input string
+$inputString = "Hello, World!";
+
+// Calling function to reverse the string
+$reversedString = reverseString($inputString);
+
+echo "Original string: $inputString<br>";
+echo "Reversed string: $reversedString";
+?>
