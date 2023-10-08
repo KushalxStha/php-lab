3dacdd53a1a6bc431bd363206980de9f52014513
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-08 20:00:16 +0545

    practical no 16 done

--- lab-16.php
+++ lab-16.php
@@ -1,1 +1,24 @@
 <!-- Give an example of paramaterized constructor in PHP -->
+
+<?php
+class Person {
+    public $name;
+    public $age;
+
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function displayInfo() {
+        echo "Name: " . $this->name . "<br>";
+        echo "Age: " . $this->age . "<br>";
+    }
+}
+
+// Create an object with a parameterized constructor
+$person1 = new Person("Kushal", 22);
+
+// Display the person's information
+$person1->displayInfo();
+?>
