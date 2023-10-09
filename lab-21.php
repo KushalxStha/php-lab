3b21b0437bf2e32476013f224e2cd925caf33d69
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-09 21:14:20 +0545

    practical no 21 done

--- lab-21.php
+++ lab-21.php
@@ -1,1 +1,22 @@
 <!-- Write a program to start and end session variable. -->
+
+<?php
+// Start a PHP session
+session_start();
+
+// Set a session variable
+$_SESSION['username'] = 'PHP';
+
+// Display the session variable
+echo "Session variable 'username' is set to: " . $_SESSION['username'] . "<br>";
+
+// End the session
+session_destroy();
+
+// Check if the session variable is still accessible after destroying the session
+if (isset($_SESSION['username'])) {
+    echo "Session variable 'username' is still set: " . $_SESSION['username'];
+} else {
+    echo "Session variable 'username' is no longer set after destroying the session.";
+}
+?>
