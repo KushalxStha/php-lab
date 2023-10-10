1788b998a79524f8a40ec081b3735d9d4a8e4384
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-10 18:29:26 +0545

    practical no 24 done

--- lab-24.php
+++ lab-24.php
@@ -1,1 +1,35 @@
 <!-- Write an example to demonstrate handling multiple exceptions. -->
+
+<?php
+class CustomException1 extends Exception {}
+class CustomException2 extends Exception {}
+
+function divideNumbers($numerator, $denominator) {
+    try {
+        if ($denominator == 0) {
+            throw new CustomException1("Division by zero is not allowed.");
+        } elseif ($denominator < 0) {
+            throw new CustomException2("Denominator should be a positive number.");
+        } else {
+            return $numerator / $denominator;
+        }
+    } catch (CustomException1 $e) {
+        echo "Error: " . $e->getMessage() . "<br>";
+    } catch (CustomException2 $e) {
+        echo "Error: " . $e->getMessage() . "<br>";
+    } catch (Exception $e) {
+        echo "Caught an unexpected exception: " . $e->getMessage() . "<br>";
+    }
+}
+
+// Test the divideNumbers function
+try {
+    $result = divideNumbers(10, 2); // No exception
+    echo "Result: $result<br>";
+
+    $result = divideNumbers(10, 0); // CustomException1
+    $result = divideNumbers(10, -5); // CustomException2
+} catch (Exception $e) {
+    echo "Caught an unexpected exception: " . $e->getMessage() . "<br>";
+}
+?>
