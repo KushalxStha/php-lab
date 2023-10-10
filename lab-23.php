1d3db993e4e5968a3a907764d854305e6aeace35
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-10 18:25:34 +0545

    practical no 23 done

--- lab-23.php
+++ lab-23.php
@@ -1,1 +1,24 @@
 <!-- Write a custom exception in PHP to confirm only students above 18 years can vote in college election. -->
+
+<?php
+class ElectionAgeException extends Exception {
+    public function errorMessage() {
+        return "Only students above 18 years of age can vote in the college election.";
+    }
+}
+
+function checkVotingEligibility($age) {
+    if ($age < 18) {
+        throw new ElectionAgeException();
+    } else {
+        echo "You are eligible to vote in the college election.";
+    }
+}
+
+try {
+    $studentAge = 15; // Change the age to test eligibility
+    checkVotingEligibility($studentAge);
+} catch (ElectionAgeException $e) {
+    echo "Error: " . $e->errorMessage();
+}
+?>
