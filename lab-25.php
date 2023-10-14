451ddfc4ca90ea6a4792b7aebdf583151ea2904e
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-14 19:27:13 +0545

    practical no 25 done

--- lab-25.php
+++ lab-25.php
@@ -1,1 +1,19 @@
 <!-- Illustrate with an example, how top level handler can handle exceptions without catch block. -->
+
+<?php
+function customExceptionHandler($exception) {
+    echo "Unhandled Exception: " . $exception->getMessage() . "<br>";
+}
+
+// Set the top-level exception handler
+set_exception_handler('customExceptionHandler');
+
+function triggerException() {
+    throw new Exception("This is an uncaught exception.");
+}
+
+// Trigger an exception without a catch block
+triggerException();
+
+echo "This will not be reached.";
+?>
