05f8f216d5976f59419a5419fe0f688492e9391d
Kushal Shrestha <shresthakushal61@gmail.com>
2023-10-15 21:59:17 +0545

    practical no 26 done

--- lab-26.php
+++ lab-26.php
@@ -1,1 +1,9 @@
 <!-- Write an example of error suppression. -->
+
+<?php
+// Intentional file error
+$my_file = @file('non_existent_file') or die ("Failed opening file: error was '" . error_get_last()['message'] . "'");
+
+// Will not issue a notice if the index $key doesn't exist.
+$value = @$cache[$key];
+?>
